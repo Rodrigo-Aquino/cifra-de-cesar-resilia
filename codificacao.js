36fbe5d367fc7b8c2a5a13dcbf72e5556227ddf1
Rodrigo Aquino <rodrigovrvr@gmail.com>
2021-09-02 16:39:30 -0300

    adicionado as fucoes dos butoes

---
+++ codificacao.js
@@ -0,0 +1,7 @@
+function cesar(){
+    window.location.href = "../cifra-de-cesar/cifracesar/cesar.html";
+}
+
+function base(){
+    window.location.href = "../cifra-de-cesar/basemeiaquatro/basemeiaquatro.html"
+}
